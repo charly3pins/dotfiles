a7670b7fc1be1a45335a1d95e33039afa9864698
charly3pins <charlyfb90@gmail.com>
2021-04-03 15:09:42 +0200

    moved spicetify to exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -8,9 +8,13 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh    "  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_D    IR/bash_completion"  # This loads nvm bash_comp    letion
 
+# LANG
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
+# SPICETIFY
+export SPICETIFY="/usr/local/spicetify-cli/spicetify"
+export PATH="$SPICETIFY:$PATH"
+
 export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$GOBIN
-
