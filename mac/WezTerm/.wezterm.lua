0cca77b8554a43f427ec423ba9a91b97a61eaefb
charly3pins <charlyfb90@gmail.com>
2024-04-27 22:00:48 +0200

    altgr 2 fix and start window maximized

--- mac/WezTerm/.wezterm.lua
+++ mac/WezTerm/.wezterm.lua
@@ -1,7 +1,14 @@
-local wezterm = require 'wezterm'
-
+local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.color_scheme = "Gruvbox dark, hard (base16)"
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return config
