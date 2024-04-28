cbc2275df4bc78ebbad6cb24ddd0e83dd6c1ed00
charly3pins <charlyfb90@gmail.com>
2024-04-28 19:33:22 +0200

    removed dracula theme for linux

--- editors/nvim/lua/plugins/lspconfig.lua
+++ editors/nvim/lua/plugins/lspconfig.lua
@@ -1,15 +1,16 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gopls = {},
-        templ = {},
-        html = {},
-        htmx = {},
-        tailwindcss = {},
-        marksman = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				gopls = {},
+				templ = {},
+				html = {},
+				htmx = {},
+				tailwindcss = {},
+				marksman = {},
+				yamlls = {},
+			},
+		},
+	},
 }
