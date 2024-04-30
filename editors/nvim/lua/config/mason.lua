34d5a0824d4db050241f09921bafe66bc50cecb1
charly3pins <charlyfb90@gmail.com>
2024-04-30 20:16:38 +0200

    mason config

---
+++ editors/nvim/lua/config/mason.lua
@@ -0,0 +1,40 @@
+local options = {
+	ensure_installed = {
+		"goimports",
+		"gopls",
+		"html-lsp",
+		"htmx-lsp",
+		"lua-language-server",
+		"marksman",
+		"shfmt",
+		"stylua",
+		"tailwindcss-language-server",
+		"templ",
+		"yaml-language-server",
+	},
+
+	PATH = "skip",
+
+	ui = {
+		icons = {
+			package_pending = " ",
+			package_installed = "󰄳 ",
+			package_uninstalled = " 󰚌",
+		},
+
+		keymaps = {
+			toggle_server_expand = "<CR>",
+			install_server = "i",
+			update_server = "u",
+			check_server_version = "c",
+			update_all_servers = "U",
+			check_outdated_servers = "C",
+			uninstall_server = "X",
+			cancel_installation = "<C-c>",
+		},
+	},
+
+	max_concurrent_installers = 10,
+}
+
+return options
