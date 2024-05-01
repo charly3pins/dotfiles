4dada5403a84c7fe86196807f3641b5e9988db18
charly3pins <charlyfb90@gmail.com>
2024-05-01 22:54:21 +0200

    never show confirmation when closing term

--- mac/WezTerm/.wezterm.lua
+++ mac/WezTerm/.wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
+config.window_close_confirmation = "NeverPrompt"
 
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
