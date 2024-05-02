ceec15ba747bbd625c406b32866b2f17e134b7a5
charly3pins <charlyfb90@gmail.com>
2024-05-02 20:05:43 +0200

    ripgrep and lazygit

--- editors/nvim/lua/config/keymaps.lua
+++ editors/nvim/lua/config/keymaps.lua
@@ -1,6 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
