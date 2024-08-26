6d547e6971ec58f1ac8ed3ed6c2a93f77d511890
charly3pins <charlyfb90@gmail.com>
2024-08-26 15:53:22 +0200

    fix identations

--- editors/nvim/lua/charly3pins/core/options.lua
+++ editors/nvim/lua/charly3pins/core/options.lua
@@ -7,8 +7,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
