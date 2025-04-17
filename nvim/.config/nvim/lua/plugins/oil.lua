ebb5a22ca6de413676c46c1c2e5b170ef3406d8a
charly3pins <carlesfb90@gmail.com>
2025-04-17 22:36:54 +0200

    oil command

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,6 @@ return {
     config = function()
         local oil = require("oil")
         oil.setup()
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        vim.keymap.set("n", "<C-_>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
 }
