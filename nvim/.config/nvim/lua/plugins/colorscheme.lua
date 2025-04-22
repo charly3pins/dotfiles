18d8d0f9de15bcac4fcf55710dfcf6218ea005ee
charly3pins <carlesfb90@gmail.com>
2025-04-23 00:01:26 +0200

    evangelion

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,23 @@
+-- return {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     config = function()
+--         require("rose-pine").setup({
+--             disable_background = true,
+--             styles = {
+--                 italic = false,
+--             },
+--         })
+--         vim.cmd.colorscheme("rose-pine-moon")
+--         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     end,
+-- }
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-        require("rose-pine").setup({
-            disable_background = true,
-            styles = {
-                italic = false,
-            },
-        })
-        vim.cmd.colorscheme("rose-pine-moon")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end,
+	"xero/evangelion.nvim",
+	lazy = false,
+	priority = 1000,
+	init = function()
+		vim.cmd.colorscheme("evangelion")
+	end,
 }
