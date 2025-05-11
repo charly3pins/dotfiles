d04a54bf18e34ca46a145036c18a8f699918764c
charly3pins <carlesfb90@gmail.com>
2025-05-11 19:39:34 +0200

    rose-pine all night long

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,15 @@
 return {
-  "xero/evangelion.nvim",
-  lazy = false,
-  init = function()
-    vim.cmd.colorscheme "evangelion"
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup {
+      disable_background = true,
+      styles = {
+        italic = false,
+      },
+    }
+    vim.cmd.colorscheme "rose-pine-moon"
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end,
 }
