c38ae38cd0448156280c9f2bc3f8258507b4f309
charly3pins <carlesfb90@gmail.com>
2025-06-21 18:43:36 +0200

    oil config

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,16 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup {}
+    require("oil").setup {
+      default_file_explorer = true,
+      columns = {},
+      keymaps = {
+        ["q"] = "actions.close",
+      },
+      delete_to_trash = true,
+      view_options = { show_hidden = true },
+      skip_confirmation_for_simple_edits = true,
+    }
 
     -- Open parent directory in current window
     vim.keymap.set("n", "<space>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
