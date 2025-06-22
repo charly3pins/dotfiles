96edfa07e1accadb0424b9ab03f1b04ffb4af24c
charly3pins <carlesfb90@gmail.com>
2025-06-22 20:48:22 +0200

    sunday time

--- nvim/.config/nvim/lua/plugins/nvim-sops.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lucidph3nx/nvim-sops",
-  event = { "BufEnter" },
-  keys = {
-    { "<leader>se", vim.cmd.SopsEncrypt, desc = "[S]ops [E]ncrypt File" },
-    { "<leader>sd", vim.cmd.SopsDecrypt, desc = "[S]ops [D]ecrypt File" },
-  },
-}
