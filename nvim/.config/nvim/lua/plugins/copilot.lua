11cda8302aa44f911ce2f13acea701ba92e09660
charly3pins <carlesfb90@gmail.com>
2025-07-02 16:32:08 +0200

    copilot or pilot, that's the question

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = "<M-.>",
+          accept_word = "<M-w>",
+          accept_line = "<M-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-/>",
+        },
+      },
+      filetypes = {
+        markdown = true,
+        yaml = true,
+        go = true,
+      },
+    },
+  },
+}
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -12,16 +12,48 @@ return {
       local cmp_select = { behavior = cmp.SelectBehavior.Insert }
       cmp.setup {
         sources = {
+          { name = "copilot" },
           { name = "nvim_lsp" },
+        },
+        window = {
+          -- Make the completion menu bordered
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        view = {
+          -- Explicitly request documentation
+          docs = { auto_open = false },
         },
         mapping = cmp.mapping.preset.insert {
           ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
           ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-y>"] = cmp.mapping.confirm { select = true },
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<Tab>"] = cmp.mapping.select_next_item { behaviour = cmp.SelectBehavior.Insert },
+          ["/"] = cmp.mapping.close(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            local copilot = require "copilot.suggestion"
+            if copilot.is_visible() then
+              copilot.accept()
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping.select_prev_item { behaviour = cmp.SelectBehavior.Insert },
-          ["<CR>"] = cmp.mapping.confirm { select = true },
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ["<C-d>"] = function()
+            if cmp.visible_docs() then
+              cmp.close_docs()
+            else
+              cmp.open_docs()
+            end
+          end,
         },
       }
     end,
