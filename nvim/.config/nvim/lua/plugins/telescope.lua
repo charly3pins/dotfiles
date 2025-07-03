6ad0719a7d81103acc2d2b8adfe25b3ab7e82b59
charly3pins <carlesfb90@gmail.com>
2025-07-04 00:34:16 +0200

    telescope default

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,17 +8,9 @@ return {
     },
     config = function()
       local telescope = require "telescope"
-      local actions = require "telescope.actions"
       telescope.setup {
         defaults = {
           path_display = { "smart" },
-          mappings = {
-            i = {
-              ["<C-k>"] = actions.move_selection_previous,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            },
-          },
         },
       }
 
