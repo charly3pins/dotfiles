3d4a5e6d06d9f02e18f5aec21e5a86cfe16ac90e
charly3pins <carlesfb90@gmail.com>
2025-07-06 21:08:48 +0200

    remove aliases

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -1,26 +1,26 @@
 # Enable aliases to be sudo’ed
-alias sudo='sudo '
+# alias sudo='sudo '
 
 # Dir
 # alias ls="eza --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias ls="eza --icons=always"
-alias ll="ls -l"
-alias la="ls -la"
+# alias ls="eza --icons=always"
+# alias ll="ls -l"
+# alias la="ls -la"
 
 #Jumps
-alias ..="cd .."
+# alias ..="cd .."
 
 # Git
-alias ga="git add"
-alias gaa="git add -A"
-alias gc="git commit"
-alias gco="git checkout"
-alias gd="git diff"
-alias gs="git status -sb"
-alias gf="git fetch --all -p"
-alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull"
-alias gl="git log --oneline"
-alias glo="git log --graph --decorate --pretty=oneline --abbrev-commit"
-alias gri="git rebase -i"
+# alias ga="git add"
+# alias gaa="git add -A"
+# alias gc="git commit"
+# alias gco="git checkout"
+# alias gd="git diff"
+# alias gs="git status -sb"
+# alias gf="git fetch --all -p"
+# alias gps="git push"
+# alias gpsf="git push --force"
+# alias gpl="git pull"
+# alias gl="git log --oneline"
+# alias glo="git log --graph --decorate --pretty=oneline --abbrev-commit"
+# alias gri="git rebase -i"
